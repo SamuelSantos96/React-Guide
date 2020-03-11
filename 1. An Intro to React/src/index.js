592bd0769756d1a3edee128d83c9f34f880c934b
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-03-11 14:54:22 +0000

    Added all the files needed for structuring the next project.
    Added the API KEY to connect to the YouTube Data API v3.

--- 1. An Intro to React/src/index.js
+++ 1. An Intro to React/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { API_KEY } from '../API_KEY';
+
+// const myKey = API_KEY;
+console.log('API_KEY: ' + API_KEY);
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hello World!</div>;
+    return <div>API_KEY</div>;
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM)
