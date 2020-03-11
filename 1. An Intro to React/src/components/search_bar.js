afa384c4cba72cd3f4f55345eacd3bae1b69013d
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-03-11 16:26:08 +0000

    Controlled Components.

--- 1. An Intro to React/src/components/search_bar.js
+++ 1. An Intro to React/src/components/search_bar.js
@@ -4,15 +4,13 @@ export class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: '' };
+        this.state = { term: 'Starting Value' };
     }
 
     render() {
         return (
             <div>
-                {/* Never use this.state.term = ..., this.setState() calls various functions inside of it for React to handle */}
-                <input onChange={event => this.setState({ term: event.target.value })} />
-                Value of the input: {this.state.term}
+                <input value={this.state.term} onChange={event => this.setState({ term: event.target.value })} />
             </div>
         );
     }
