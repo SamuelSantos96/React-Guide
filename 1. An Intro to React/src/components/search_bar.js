27f5f3aa838fa0051b246b439719b485268f4228
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-03-11 15:48:44 +0000

    Handling User Events.

--- 1. An Intro to React/src/components/search_bar.js
+++ 1. An Intro to React/src/components/search_bar.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export class SearchBar extends Component {
     render() {
-        return <input />;
+        return <input onChange={this.onInputChange} />;
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
     }
 }
