5b4e1dfd52b73761a2f04d41bf32e97a691ccf26
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-03-11 15:07:29 +0000

    Insert component 'SearchBar' into 'App'.

--- 1. An Intro to React/src/index.js
+++ 1. An Intro to React/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { API_KEY } from '../API_KEY';
 
-// const myKey = API_KEY;
-console.log('API_KEY: ' + API_KEY);
+import { API_KEY } from '../API_KEY';
+import { SearchBar } from './components/search_bar';
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>API_KEY</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM)
