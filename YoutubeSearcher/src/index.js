ddbe4b03c83524691b75d7b9fd1c222cb06eb88b
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-03-13 18:58:04 +0000

    Youtube API Search Implementation.

--- YoutubeSearcher/src/index.js
+++ YoutubeSearcher/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import { API_KEY } from '../API_KEY';
 import { SearchBar } from './components/search_bar';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, data => {
+    console.log(data);
+});
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
