c874f32b1cb6204c1a8924aec5e7ab28ee8aadf7
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-03-17 11:32:26 +0000

    Searching for Videos.

--- YoutubeSearcher/src/index.js
+++ YoutubeSearcher/src/index.js
@@ -13,7 +13,11 @@ class App extends Component {
 
         this.state = { videos: [], selectedVideo: null };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term }, videos => {
             this.setState({ videos, selectedVideo: videos[0] });
         });
     }
@@ -21,7 +25,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
